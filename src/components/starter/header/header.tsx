c66943b77fe142062d35bc60122eefc9150af649
Mike M <watkajtys@gmail.com>
2023-06-28 00:52:13 -0600

    new header and styling

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,33 +1,44 @@
 import { component$ } from '@builder.io/qwik';
-import { QwikLogo } from '../icons/qwik';
-import styles from './header.module.css';
+import styles       from './header.module.css';
 
 export default component$(() => {
   return (
     <header class={styles.header}>
-      <div class={['container', styles.wrapper]}>
-        <div class={styles.logo}>
-          <a href="/" title="qwik">
-            <QwikLogo height={50} width={143} />
-          </a>
+      <div class={[styles.wrapper]}>
+
+        <div class={styles.logowrap}>
+            <a href="https://goldentriangleofdenver.com/" title="Golden Triangle Creative District">
+              {/* eslint-disable-next-line qwik/jsx-img */}
+              <img src="/media/logo.png" alt="GTCD Logo" class={styles.logoimage}/>
+            </a>
+          <div class={styles.event_wrapper}>
+            <div class={styles.breaker}>
+              <h2 class={'luloFont'}>COLORCON</h2>
+              <h3 class={'luloFont'}>arts & music festival</h3>
+            </div>
+            <div class={styles.breaker}>
+              <p>august 26 | 5-8pm</p>
+              <a href="https://goo.gl/maps/tLfMJBcdCssppLYM8">
+                <p>1278 Lincoln St.</p>
+              </a>
+            </div>
+          </div>
         </div>
-        <ul>
-          <li>
-            <a href="https://qwik.builder.io/docs/components/overview/" target="_blank">
-              Docs
-            </a>
-          </li>
-          <li>
-            <a href="https://qwik.builder.io/examples/introduction/hello-world/" target="_blank">
-              Examples
-            </a>
-          </li>
-          <li>
-            <a href="https://qwik.builder.io/tutorial/welcome/overview/" target="_blank">
-              Tutorials
-            </a>
-          </li>
-        </ul>
+
+        <div class={styles.menu}>
+          <ul>
+            <li>
+              <a href="https://qwik.builder.io/docs/components/overview/" target="_blank">
+                Artists
+              </a>
+            </li>
+            <li>
+              <a href="https://goldentriangleofdenver.wildapricot.org/event-5269261" target="_blank" class={styles.button}>
+                TICKETS
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
