1c6ffac4da0bbbb7e2c80f6293d970be18951b36
Mike M <watkajtys@gmail.com>
2023-06-29 18:49:57 -0600

    adding another image pair

--- src/components/starter/about-images/about-images.tsx
+++ src/components/starter/about-images/about-images.tsx
@@ -11,7 +11,16 @@ export default component$(() => {
                         community. making art accessible to all</p>
                 </div>
 
-                <img class={styles.imagebox} src="/media/colorcan.png" alt="colorcon can art"/>
+                <img class={styles.imagebox} src="/media/colorcan.png" alt="colorcon can art" width="600" height="600"/>
+            </div>
+
+            <div class={styles.aboutitem}>
+                <div class={styles.aboutcopy}>
+                    <h3 class={'luloFont'}>Making Place</h3>
+                    <p class={'big'}>re-imagining spaces/alleyways in the Golden Triangle Creative District.</p>
+                </div>
+
+                <img class={styles.imagebox} src="/media/create.png" alt="colorcon can art" width="600" height="600"/>
             </div>
         </div>
     );
