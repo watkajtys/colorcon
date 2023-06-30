c3c43a85a7c4aac7e98b12d6165b1e3c51375708
Mike M <watkajtys@gmail.com>
2023-06-29 18:42:28 -0600

    squelching small crawl errors. Adding explicit width/height to images

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -9,7 +9,7 @@ export default component$(() => {
         <div class={styles.logowrap}>
             <a href="https://goldentriangleofdenver.com/" title="Golden Triangle Creative District">
               {/* eslint-disable-next-line qwik/jsx-img */}
-              <img src="/media/logo.png" alt="GTCD Logo" class={styles.logoimage}/>
+              <img src="/media/logo.png" alt="GTCD Logo" class={styles.logoimage} width={"50"} height={"50"}/>
             </a>
           <div class={styles.event_wrapper}>
             <div class={styles.breaker}>
