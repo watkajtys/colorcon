00ecf1d742e5f7a4692022f4f527fa0a08d686f7
Mike M <watkajtys@gmail.com>
2023-07-10 13:12:27 -0400

    shifting to box model

--- src/components/starter/about-images/about-images.tsx
+++ src/components/starter/about-images/about-images.tsx
@@ -1,27 +1,50 @@
 import {component$} from '@builder.io/qwik';
 import styles       from './about-images.module.css';
+import Box          from "~/components/starter/about-images/box";
+import ColorBox from '~/media/colorcan.png?jsx';
+import CreateImage from '~/media/create.png?jsx';
+import DreamImage from '~/media/dreambig.png?jsx';
+import MusicImage from '~/media/music.png?jsx';
 
 export default component$(() => {
     return (
         <div class="container container-flex column">
-            <div class={styles.aboutitem}>
-                <div class={styles.aboutcopy}>
-                    <h3 class={'luloFont'}>What Is Colorcon?</h3>
-                    <p class={'big'}>COLORCON is a musical and artistic mural event that moves throughout the GTCD
-                        community. making art accessible to all</p>
+
+            <Box alt={true}>
+                <div q:slot={"title"}>What Is ColorCon?</div>
+                <p q:slot={"copy"} class={'big'}>COLORCON is a musical and artistic mural event that moves throughout the GTCD
+                    community. making art accessible to all</p>
+                <a q:slot={"btn"} href={'www.google.com'} class={['button', 'alt']}>Google</a>
+                <div q:slot={"image"}>
+                    <ColorBox class={styles.imagebox}/>
                 </div>
+            </Box>
 
-                <img class={styles.imagebox} src="/media/colorcan.png" alt="colorcon can art" width="600" height="600"/>
-            </div>
-
-            <div class={styles.aboutitem}>
-                <div class={styles.aboutcopy}>
-                    <h3 class={'luloFont'}>Making Place</h3>
-                    <p class={'big'}>re-imagining spaces/alleyways in the Golden Triangle Creative District.</p>
+            <Box>
+                <div q:slot={"title"}>Making Place</div>
+                <p q:slot={"copy"} class={'big'}>re-imagining spaces/alleyways in the Golden Triangle Creative District.</p>
+                <div q:slot={"image"}>
+                    <CreateImage class={styles.imagebox}/>
                 </div>
+            </Box>
 
-                <img class={styles.imagebox} src="/media/create.png" alt="colorcon can art" width="600" height="600"/>
-            </div>
+            <Box alt={true}>
+                <div q:slot={"title"}>What Is ColorCon?</div>
+                <p q:slot={"copy"} class={'big'}>COLORCON is a musical and artistic mural event that moves throughout the GTCD
+                    community. making art accessible to all</p>
+                <a q:slot={"btn"} href={'www.google.com'} class={['button', 'alt']}>Google</a>
+                <div q:slot={"image"}>
+                    <DreamImage class={styles.imagebox}/>
+                </div>
+            </Box>
+
+            <Box>
+                <div q:slot={"title"}>Celebrate Culture</div>
+                <p q:slot={"copy"} class={'big'}>COLORCON brings art and music to life in the Golden Triangle</p>
+                <div q:slot={"image"}>
+                    <MusicImage class={styles.imagebox}/>
+                </div>
+            </Box>
         </div>
     );
 });
