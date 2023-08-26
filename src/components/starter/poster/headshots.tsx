377cd254f77388fbc401a8075b0695c0485d5c98
Mike M <watkajtys@gmail.com>
2023-08-26 09:54:07 -0600

    add another headshot and refloat

--- src/components/starter/poster/headshots.tsx
+++ src/components/starter/poster/headshots.tsx
@@ -6,15 +6,17 @@ import Headshot2 from '~/media/headshot/hannimal.png?jsx';
 import Headshot3 from '~/media/headshot/holmes2.png?jsx';
 import Headshot4 from '~/media/headshot/mello2.png?jsx';
 import Headshot5 from '~/media/headshot/will2.png?jsx';
+import Headshot6 from '~/media/headshot/marco.png?jsx';
 
 export default component$(() => {
     return (
         <div class={styles.headshots}>
-            <Headshot1 class={styles.headshotimage} alt={'Carlos Fresquez headshot'}/>
+            <Headshot5 class={styles.headshotimage} alt={'Will Barker headshot'}/>
             <Headshot2 class={styles.headshotimage} alt={'Mr. Hanimal headshot'}/>
+            <Headshot1 class={styles.headshotimage} alt={'Carlos Fresquez headshot'}/>
             <Headshot3 class={styles.headshotimage} alt={'Jasmine Holmes headshot'}/>
             <Headshot4 class={styles.headshotimage} alt={'RJ Mello headshot'}/>
-            <Headshot5 class={styles.headshotimage} alt={'Will Barker headshot'}/>
+            <Headshot6 class={styles.headshotimage} alt={'Marco Garcia headshot'}/>
         </div>
     );
 });
