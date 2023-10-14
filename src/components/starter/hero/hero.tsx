2a7a6bd1dbc220ccff60c87e3f3e95db6c6a3128
Mike M <watkajtys@gmail.com>
2023-10-14 15:04:54 -0600

    test fix

--- src/components/starter/hero/hero.tsx
+++ src/components/starter/hero/hero.tsx
@@ -5,9 +5,8 @@ export default component$(() => {
   return (
     <div class={[styles.hero]}>
       <h1>
-        <span class="highlight">COLORCON 2023</span>
+        <span class="highlight">IZK</span>
       </h1>
-      <p>Celebrating Local Icons Past & Present</p>
       <div class={styles['button-group']}>
         <a href="https://goldentriangleofdenver.wildapricot.org/event-5269261" target="_blank" class="button">
           TICKETS
